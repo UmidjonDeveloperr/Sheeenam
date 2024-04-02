d0a83e8427bfc8a90b9b8d689443ad75cb145a8d
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-04-02 11:57:30 +0500

    DATA: Model & Migrate Guests

---
+++ Sheeenam.Api/Models/Foundations/Guests/Guest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sheeenam.Api.Models.Foundations.Guests
+{
+	public class Guest
+	{
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirthday { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public GenderType Gender { get; set; }
+    }
+}
