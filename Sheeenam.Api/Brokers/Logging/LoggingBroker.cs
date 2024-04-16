bfb0cd7032bf0fb53f4f22e08fd56edfb50e03c6
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-04-16 21:06:55 +0500

    BROKERS: Implement Logging

---
+++ Sheeenam.Api/Brokers/Logging/LoggingBroker.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Sheeenam.Api.Brokers.Logging
+{
+	public class LoggingBroker : ILoggingBroker
+	{
+		private readonly ILogger<ILoggingBroker> logger;
+
+		public LoggingBroker(ILogger<ILoggingBroker> logger) =>
+			this.logger = logger;
+
+		public void LogError(Exception exception) =>
+			this.logger.LogError(exception, exception.Message);
+
+		public void LogCritical(Exception exception) =>
+			this.logger.LogCritical(exception, exception.Message);
+	}
+}
