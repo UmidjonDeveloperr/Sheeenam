4e45432c4823e54899652ed5af8d1a29c54dcbbd
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-04-17 23:42:28 +0500

    CODE RUB: Logging Example

---
+++ Sheeenam.Api.Tests.xUnit/Services/Foundations/Guests/GuestServiceTests.Logic.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheeenam.Api.Models.Foundations.Guests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sheeenam.Api.Tests.xUnit.Services.Foundations.Guests
+{
+	public partial class GuestServiceTests
+	{
+		[Fact]
+		public async Task ShouldAddGuestAsync()
+		{
+			//given
+			Guest randomGuest = CreateRandomGuest();
+			Guest inputGuest = randomGuest;
+			Guest storageGuest = inputGuest;
+			Guest expectedGuest = storageGuest.DeepClone();
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.InsertGuestAsync(inputGuest)).
+					ReturnsAsync(storageGuest);
+
+			//formula
+			Guest actualGuest = 
+				await this.guestService.AddGuestAsync(inputGuest);
+
+			//yechim
+			actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.InsertGuestAsync(inputGuest), Times.Once());
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			
+
+		}
+	}
+}
---
+++ Sheeenam.Api/Services/Foundation/Guests/GuestService.cs
@@ -0,0 +1,19 @@
+﻿using Sheeenam.Api.Brokers.Storages;
+using Sheeenam.Api.Models.Foundations.Guests;
+using System.Threading.Tasks;
+
+namespace Sheeenam.Api.Services.Foundation.Guests
+{
+	public class GuestService : IGuestService
+	{
+		private readonly IStorageBroker storageBroker;
+        public GuestService(IStorageBroker storageBroker) =>
+			this.storageBroker = storageBroker;
+
+        public async ValueTask<Guest> AddGuestAsync(Guest guest)
+		{
+
+			return await this.storageBroker.InsertGuestAsync(guest);
+		}
+	}
+}
