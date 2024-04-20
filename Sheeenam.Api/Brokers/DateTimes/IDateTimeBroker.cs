43806f6dbefddbefeb5447927933ce69295183c8
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-04-20 15:23:31 +0500

    SERVICE: Service Crud

---
+++ Sheeenam.Api/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Sheeenam.Api.Brokers.DateTimes
+{
+	public interface IDateTimeBroker
+	{
+		DateTimeOffset GetCurrentDateTime();
+	}
+}
---
+++ Sheeenam.Api/Models/Foundations/Guests/Exceptions/NotFoundGuestException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace Sheeenam.Api.Models.Foundations.Guests.Exceptions
+{
+	public class NotFoundGuestException : Xeption
+	{
+        public NotFoundGuestException(Guid guestId)
+            :base(message: $"Could not find Guest with id:{guestId}")
+        { }
+    }
+}
